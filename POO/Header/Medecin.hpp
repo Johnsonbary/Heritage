b7f304e78a70b9888d46d30b77c1d7495291ed36
Terence <167604947+Johnsonbary@users.noreply.github.com>
2025-02-12 09:06:13 +0300

    Update Medecin.hpp

--- POO/Header/Medecin.hpp
+++ POO/Header/Medecin.hpp
@@ -2,24 +2,20 @@
 #define CLASS_MEDECIN
 
 #include <string>
-#include "../Header/Person.hpp"  // Supposons que Person est une autre classe dont Medecin hérite
+#include "../Header/Person.hpp"  
 using namespace std;
 namespace humain
 {
 class Medecin : public Person {
 private:
     string Speciality;
-    string Grade;  // infirmiere, docteur, interne qualifiant, professeur
+    string Grade;  
 
 public:
-    // Constructeur
+    
     Medecin(string , string , string , string , int);
-
-    // Setters
     void setSpetiality(string speciality);
     void setGrade(string grade);
-
-    // Getters
     string getSpetiality();
     string getGrade();
     void Presentation();
